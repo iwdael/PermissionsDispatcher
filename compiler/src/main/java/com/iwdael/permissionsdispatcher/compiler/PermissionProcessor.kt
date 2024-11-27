7d896d830b6ad7f14d74f2a0f44b4b00d68e73e2
iwdael <iwdael@outlook.com>
2024-11-27 11:07:05 +0800

    Update Processor.kt

--- compiler/src/main/java/com/iwdael/permissionsdispatcher/compiler/PermissionProcessor.kt
+++ compiler/src/main/java/com/iwdael/permissionsdispatcher/compiler/PermissionProcessor.kt
@@ -1,11 +1,9 @@
 package com.iwdael.permissionsdispatcher.compiler
 
-import com.google.devtools.ksp.processing.CodeGenerator
-import com.google.devtools.ksp.processing.KSPLogger
 import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.processing.SymbolProcessor
+import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.symbol.KSAnnotated
-import com.google.devtools.ksp.validate
 import com.iwdael.kotlinsymbolprocessor.asKspClass
 import com.iwdael.permissionsdispatcher.annotation.PermissionsDispatcher
 
@@ -13,15 +11,19 @@ import com.iwdael.permissionsdispatcher.annotation.PermissionsDispatcher
  * author : iwdael
  * e-mail : iwdael@outlook.com
  */
-class PermissionProcessor(private val codeGenerator: CodeGenerator, private val logger: KSPLogger) :
+class PermissionProcessor(private val env: SymbolProcessorEnvironment) :
     SymbolProcessor {
     override fun process(resolver: Resolver): List<KSAnnotated> {
         resolver.getSymbolsWithAnnotation(PermissionsDispatcher::class.java.name)
-//            .filter { it.validate() }
             .map { it.asKspClass }
             .map { Permission(it) }
             .map { PermissionGenerator(it) }
-            .forEach { it.write(codeGenerator) }
+            .forEach {
+                try {
+                    it.write(env.codeGenerator)
+                } catch (_: Exception) {
+                }
+            }
         return emptyList()
     }
 }
