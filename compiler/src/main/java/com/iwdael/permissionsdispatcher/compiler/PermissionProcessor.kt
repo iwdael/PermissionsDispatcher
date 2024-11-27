0a378c410fd083d3e5632cae2a5b010301d498cd
iwdael <iwdael@outlook.com>
2024-11-27 11:07:05 +0800

    Update Processor.kt

--- compiler/src/main/java/com/iwdael/permissionsdispatcher/compiler/PermissionProcessor.kt
+++ compiler/src/main/java/com/iwdael/permissionsdispatcher/compiler/PermissionProcessor.kt
@@ -1,10 +1,15 @@
 package com.iwdael.permissionsdispatcher.compiler
 
-import com.google.devtools.ksp.processing.CodeGenerator
-import com.google.devtools.ksp.processing.KSPLogger
+import com.google.devtools.ksp.KspExperimental
+import com.google.devtools.ksp.containingFile
+import com.google.devtools.ksp.isAnnotationPresent
+import com.google.devtools.ksp.processing.Dependencies
 import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.processing.SymbolProcessor
+import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.symbol.KSAnnotated
+import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.symbol.KSFile
 import com.google.devtools.ksp.validate
 import com.iwdael.kotlinsymbolprocessor.asKspClass
 import com.iwdael.permissionsdispatcher.annotation.PermissionsDispatcher
@@ -13,15 +18,23 @@ import com.iwdael.permissionsdispatcher.annotation.PermissionsDispatcher
  * author : iwdael
  * e-mail : iwdael@outlook.com
  */
-class PermissionProcessor(private val codeGenerator: CodeGenerator, private val logger: KSPLogger) :
-    SymbolProcessor {
+class PermissionProcessor(private val env: SymbolProcessorEnvironment) : SymbolProcessor {
+    private var processed = false
+
+    @OptIn(KspExperimental::class)
     override fun process(resolver: Resolver): List<KSAnnotated> {
-        resolver.getSymbolsWithAnnotation(PermissionsDispatcher::class.java.name)
-//            .filter { it.validate() }
-            .map { it.asKspClass }
-            .map { Permission(it) }
-            .map { PermissionGenerator(it) }
-            .forEach { it.write(codeGenerator) }
+        if (processed) return emptyList()
+        processed = true
+        resolver.getAllFiles()
+            .toList()
+            .flatMap { it.declarations }
+            .filter { it.isAnnotationPresent(PermissionsDispatcher::class) }
+            .onEach {
+                val dependencies = Dependencies(aggregating = false, it.containingFile!!)
+                PermissionGenerator(Permission(it.asKspClass)).write(env.codeGenerator, dependencies)
+            }
         return emptyList()
     }
+
+
 }
